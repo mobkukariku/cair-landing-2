b01fb780d667c28c0c23b5d0586be9ade7093c89
Damir Tagankhozhaev <tagankhozhaev@gmail.com>
2025-10-15 21:19:54 +0500

    feat:added header

---
+++ src/sections/main/ui/MainSection.tsx
@@ -0,0 +1,19 @@
+export function MainSection() {
+    return (
+        <section>
+            <div>
+                <h4>A reliable IT partner you’ll want by your side.</h4>
+                <h1>
+                    Shaping your
+                    visions with a spark
+                    of innovation
+                </h1>
+            </div>
+            <div>
+                <p>Comprehensive support for medium and large companies</p>
+                <p>We help businesses run more efficiently while enhancing support quality</p>
+                <button >Contact</button>
+            </div>
+        </section>
+    )
+}
