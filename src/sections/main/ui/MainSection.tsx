6d403af98c345aef4c45f789d80011dc2e26ae8c
Damir Tagankhozhaev <tagankhozhaev@gmail.com>
2025-10-22 01:57:08 +0500

    ref: changed percents

--- src/sections/main/ui/MainSection.tsx
+++ src/sections/main/ui/MainSection.tsx
@@ -3,7 +3,7 @@ import { HeroText } from "./HeroText";
 
 export function MainSection() {
     return (
-        <section id={"home"} className="text-white h-[90%] w-full flex flex-col justify-between items-center">
+        <section id={"home"} className="text-white h-[87%] w-full flex flex-col justify-between items-center">
             <div className="flex-grow flex flex-col items-center w-full gap-30 justify-center">
                 <HeroText />
             </div>
