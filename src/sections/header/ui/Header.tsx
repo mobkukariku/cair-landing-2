6f1c96323a4d8358dc7843c2226dea031115b6b6
Damir Tagankhozhaev <tagankhozhaev@gmail.com>
2025-11-02 13:47:07 +0500

    fixed2

--- src/sections/header/ui/Header.tsx
+++ src/sections/header/ui/Header.tsx
@@ -1,12 +1,18 @@
-"use client"
+"use client";
 import {useMediaQuery} from "@/hooks/useMediaQuery";
 import {MobileHeader} from "@/sections/header/ui/MobileHeader";
 import {DesktopHeader} from "@/sections/header/ui/DesktopHeader";
+import {useEffect, useState} from "react";
 
 export function Header() {
-
+    const [hasMounted, setHasMounted] = useState(false);
     const isMobile = useMediaQuery("(max-width: 768px)");
 
-    return isMobile ? <MobileHeader /> : <DesktopHeader />;
+    useEffect(() => {
+        setHasMounted(true);
+    }, []);
 
+    if (!hasMounted) return null;
+
+    return isMobile ? <MobileHeader /> : <DesktopHeader />;
 }
